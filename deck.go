33c58c9884a06e5f4686bc70dbff929e2457aa6f
Thomas Latortue <tsl13b@gmail.com>
2018-08-21 22:03:27 -0400

    add suits and values

--- deck.go
+++ deck.go
@@ -3,3 +3,27 @@ package main
 // create a new type of 'deck' which is a slice of strings
 
 type deck []string
+
+// function returns a value of type deck
+func newDeck() deck {
+	// new var of cards type deck
+	// two slices. one of card suits, one of card values
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// replace unused variables (i & j) with _ in golang
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+}
+
+// receiver function (var d type 'deck')
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
